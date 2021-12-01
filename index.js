c10a9d22871fefff1ea540f2fb5c506bd0e48b11
Deng <donggdeng@gmail.com>
2021-12-01 19:26:28 +0800

    Create app and messing around with docker

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const mongoose = require('mongoose');
+
+const app = express()
+
+mongoose.connect(
+    "mongodb://deng:mypassword@mongo:27017/?authSource=admin"
+).then(() => console.log("successfully connected to DB"))
+.catch((e) => console.log(e))
+
+app.get("/", (req, res) => {
+    res.send("<h2>Hi there!!!sdfsd</h2>");
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`))
